1e5c95ffb3615d72e406fbb90271f8566e48a4f8
Kaan Doğan <wowfalcon535@gmail.com>
2025-06-27 15:28:36 +0300

    shopier

---
+++ cftctileceramic/assets/js/deneme.js
@@ -0,0 +1,15 @@
+
+
+
+var productPrice = 1;
+
+var productName = "Tabak";
+
+myProduct1 = { Price: 10, Name: "Tabak" };
+myProduct2 = { Price: 20, Name: "Çömlek" };
+myProduct3 = { Price: 30, Name: "Vazo" };
+myProduct4 = { Price: 40, Name: "Çatal" };
+
+totalSepet = myProduct1.Price + myProduct2.Price + myProduct3.Price;
+
+console.log("Toplam Sepet Tutarı: " + totalSepet);
