9d5d9f2a8d9e378f9177777a80326a8d77013b2d
Kaan Doğan <wowfalcon535@gmail.com>
2025-07-03 13:58:40 +0300

    Update scraper.js
    123

--- cftctileceramic/Shopier-backend/scraper.js
+++ cftctileceramic/Shopier-backend/scraper.js
@@ -3,7 +3,11 @@ const cheerio = require('cheerio');
 
 const shopierUrl = 'https://www.shopier.com/ShowProductNew/storefront.php?shop=cftctileceramic';
 
-axios.get(shopierUrl).then((response) => {
+axios.get(shopierUrl, {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/115 Safari/537.36'
+  }
+}).then((response) => {
   const $ = cheerio.load(response.data);
   $('.product-card').each((i, el) => {
     const title = $(el).find('.product-title').text().trim();
@@ -14,3 +18,5 @@ axios.get(shopierUrl).then((response) => {
     // Bu verileri kendi sitene DB'ye kaydet
   });
 });
+
+  
