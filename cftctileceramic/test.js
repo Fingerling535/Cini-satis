2f297643e1dcf16f106d4931f16fc157d2ce159b
Kaan Doğan <wowfalcon535@gmail.com>
2025-07-04 21:37:34 +0300

    scrap post done!

--- cftctileceramic/test.js
+++ cftctileceramic/test.js
@@ -1,18 +1,21 @@
     function loadProducts() {   
-    fetch('products.json')
+    fetch('cftctileceramic/products.json')
       .then(res => res.json())
       .then(data => {
-        const container = document.getElementById('product-container');
-        data.forEach(product => {
-          const card = document.createElement('div');
-          card.classList.add('product-card');
-          card.innerHTML = `
-            <img src="${product['product-image-1']}" alt="${product['product-title']}">
-            <h3>${product['product-title']}</h3>
-            <p>${product['product-price-try']} TL</p>          
-          `;
-          container.appendChild(card);
-        });
-      });
-    }
-    loadProducts();
+        console.log(data);
+    });
+  }
+    //     const container = document.getElementById('product-container');
+    //     data.forEach(product => {
+    //       const card = document.createElement('div');
+    //       card.classList.add('product-card');
+    //       card.innerHTML = `
+    //         <img src="${product['product-image-1']}" alt="${product['product-title']}">
+    //         <h3>${product['product-title']}</h3>
+    //         <p>${product['product-price-try']} TL</p>          
+    //       `;
+    //       container.appendChild(card);
+    //     });
+    //   });
+    // }
+    // loadProducts();
