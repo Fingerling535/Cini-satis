0539682491a30eb548f54d56a068c006e6028650
Kaan Doğan <wowfalcon535@gmail.com>
2025-07-07 17:15:30 +0300

    123

--- cftctileceramic/test.js
+++ cftctileceramic/test.js
@@ -1,41 +1,42 @@
-function loadProducts() {
-  const products = document.querySelector('.product-card');
+   function loadProducts() {
+      const products = document.querySelector('.product-card');
 
          // JSON dosyasını yükleme
          
-      fetch('./products.json')
-      .then(response => response.json())
-      .then(products => {
-         const container = document.getElementById('product-container');
-         const tpl = document.getElementById('product-template').content;
-         
-         const countDiv = document.querySelector('.product-item-count');
-
-
-         products.forEach(prod =>{
-            
-            //template içeriğini kopyalama
-            const clone = document.importNode(tpl, true);
-            const link = clone.querySelector('.product-card-link');
-            link.href = prod['product-link'];
-            // Kopyalanan içeriği products div'ine ekleme
-            clone.querySelector('.thumbitem-primary').src = prod['product-image-1'];
-            // Eğer ikinci resim varsa onu da yükle - Tek koymak istersen else kısmını kaldırabilirsin
-            if(prod['product-image-2']) {
-
-               clone.querySelector('.thumbitem-secondary').src = prod['product-image-2'];
-            } else {
-               clone.querySelector('.tpproduct__title').textContent = prod['product-title'];
-               clone.querySelector('.tpproduct__ammount-try span').textContent = prod['product-price-try'];
+         fetch('./products.json')
+            .then(response => response.json())
+            .then(products => {
+               const container = document.getElementById('product-container');
+               const tpl = document.getElementById('product-template').content;
                
-               countDiv.textContent = `Toplam ${products.length} ürün gösteriliyor.`;
+               const countDiv = document.querySelector('.product-item-count');
 
-         }
-            // Ürünü container'a ekleme
-            container.appendChild(clone);
-         });
 
-         console.log(products);
-      })
-      .catch(err => console.error('Error loading products:', err));
-}
+               products.forEach(prod =>{
+                  
+                  //template içeriğini kopyalama
+                  const clone = document.importNode(tpl, true);
+                  const link = clone.querySelector('.product-card-link');
+                  link.href = prod['product-link'];
+                  // Kopyalanan içeriği products div'ine ekleme
+                  clone.querySelector('.thumbitem-primary').src = prod['product-image-1'];
+                  // Eğer ikinci resim varsa onu da yükle - Tek koymak istersen else kısmını kaldırabilirsin
+                  if(prod['product-image-2']) {
+
+                     clone.querySelector('.thumbitem-secondary').src = prod['product-image-2'];
+                  } else {
+                     clone.querySelector('.tpproduct__title').textContent = prod['product-title'];
+                     clone.querySelector('.tpproduct__ammount-try span').textContent = prod['product-price-try'];
+                    
+                     countDiv.textContent = `Toplam ${products.length} ürün gösteriliyor.`;
+
+               }
+                  // Ürünü container'a ekleme
+                  container.appendChild(clone);
+               });
+
+               console.log(products);
+            })
+            .catch(err => console.error('Error loading products:', err));
+   }
+      loadProducts();
