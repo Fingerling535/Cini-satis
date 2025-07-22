42ae1442638bd33fbd6bcfece0d0fae6730683f8
Erkan Çiftçi <erkancftc@gmail.com>
2025-07-22 13:54:48 +0300

    deploy için eklenen dosyalar

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('cftctileceramic'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'cftctileceramic', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
