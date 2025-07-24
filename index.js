5016d1a5d42a00b4b1c37d89bf2989483a4f000f
Kaan Doğan <wowfalcon535@gmail.com>
2025-07-24 13:46:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//   res.send('Çini satış backend çalışıyor!');
-// });
+// Serve static files from the project root
+app.use(express.static(__dirname));
+
+// Optionally, serve index.html on root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor`);
