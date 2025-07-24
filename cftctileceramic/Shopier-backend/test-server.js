2e99e7ea9bc3fbbb0f8186e014d4c9b5ef8bd9c4
Kaan Doğan <wowfalcon535@gmail.com>
2025-07-24 13:57:21 +0300

    deleting file

--- cftctileceramic/Shopier-backend/test-server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.use(express.static('cftctileceramic')); // public klasör
-
-app.listen(PORT, () => {
-  console.log(`✅ Sunucu çalışıyor: http://localhost:${PORT}`);
-});
