75e3af740352124dfb4ea7357099b3d07c71faeb
Kaan Doğan <wowfalcon535@gmail.com>
2025-07-24 13:44:28 +0300

    123

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Çini satış backend çalışıyor!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Çini satış backend çalışıyor!');
+// });
 
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor`);
